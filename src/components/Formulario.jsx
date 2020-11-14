76461d1c9f0dbf3fd29c9742a1cda23913d7e9f8
Rafael Estrada <restradap1600@gmail.com>
2020-11-14 11:10:04 -0600

    Se añadió un state local a Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Formulario = () => {
+
+    // Crear state de citas
+    const [cita, actualizarCita] = useState({
+        mascota: "",
+        propietario: "",
+        fecha: "",
+        hora: "",
+        sintomas: ""
+    });
+
+    // Cada vez que el usuario escribe en un input
+    const actualizarState = () => {
+        console.log("escribiendo...");
+    }
+
     return (
         <Fragment>
             <h2>Crear Cita</h2>
@@ -11,6 +26,7 @@ const Formulario = () => {
                     name = "mascota"
                     className = "u-full-width"
                     placeholder = "Nombre de la mascota..."
+                    onChange = { actualizarState }
                 />
                 <label>Dueño</label>
                 <input 
@@ -18,24 +34,28 @@ const Formulario = () => {
                     name = "propietario"
                     className = "u-full-width"
                     placeholder = "Nombre del dueño de la mascota..."
+                    onChange = { actualizarState }
                 />
                 <label>Fecha</label>
                 <input 
                     type = "date"
                     name = "fecha"
                     className = "u-full-width"
+                    onChange = { actualizarState }
                 />
                 <label>Hora</label>
                 <input 
                     type = "time"
                     name = "hora"
                     className = "u-full-width"
+                    onChange = { actualizarState }
                 />
                 <label>Síntomas</label>
                 <textarea
                     className = "u-full-width"
                     name = "sintomas"
                     placeholder = "Descripción de los sintomas..."
+                    onChange = { actualizarState }
                 ></textarea>
                 <button
                     type = "submit"
