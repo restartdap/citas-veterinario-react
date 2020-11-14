f8255c5bf74fc8c6d8432e35d25041bbc66c3701
Rafael Estrada <restradap1600@gmail.com>
2020-11-14 10:59:19 -0600

    Se añadió código al componente Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Formulario = () => {
     return (
-        <h1>Desde Formulario</h1>
+        <Fragment>
+            <h2>Crear Cita</h2>
+            <form>
+                <label>Mascota</label>
+                <input 
+                    type = "text"
+                    name = "mascota"
+                    className = "u-full-width"
+                    placeholder = "Nombre de la mascota..."
+                />
+                <label>Dueño</label>
+                <input 
+                    type = "text"
+                    name = "propietario"
+                    className = "u-full-width"
+                    placeholder = "Nombre del dueño de la mascota..."
+                />
+                <label>Fecha</label>
+                <input 
+                    type = "date"
+                    name = "fecha"
+                    className = "u-full-width"
+                />
+                <label>Hora</label>
+                <input 
+                    type = "time"
+                    name = "hora"
+                    className = "u-full-width"
+                />
+                <label>Síntomas</label>
+                <textarea
+                    className = "u-full-width"
+                    name = "sintomas"
+                    placeholder = "Descripción de los sintomas..."
+                ></textarea>
+                <button
+                    type = "submit"
+                    className = "u-full-width button-primary"
+                >Agregar Cita</button>
+            </form>
+        </Fragment>
     );
 };
 
